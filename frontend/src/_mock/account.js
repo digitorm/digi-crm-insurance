dcd6e7754a736980e30bb7d7136aa7ae0d95f162
Subhan Saleem <mk.subhansaleem@gmail.com>
2023-12-05 12:45:06 +0500

    first showcase

---
+++ frontend/src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Jaydon Frankie',
+  email: 'demo@app.cc',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
